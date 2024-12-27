6a8b5cb324cdbc9fbd0513f143a56c253530d3d2
Chiko <chiko@envs.net>
2024-12-27 17:27:44 +0000

    chore: init app

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "all",
+  content: ["./app/src/**/*.{rs,html,css}", "./app/dist/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
